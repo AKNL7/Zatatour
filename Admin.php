daae40c7b9ec4b65e516d093ea01e3fc12391c10
octador <mutuon.j@gmail.com>
2024-02-14 14:28:27 +0100

    start class

---
+++ Admin.php
@@ -0,0 +1,9 @@
+<?php
+
+class Admin{
+private $db;
+public function __construct($db){
+$this->db= $db ;
+}
+
+}
--- class/TourOperator.php
+++ class/TourOperator.php
@@ -4,10 +4,81 @@ class tourOperator{
     private $id;
     private $name;
     private $link;
-    private$gradeCount;
+    private $gradeCount;
     private $gradeTotalprivate;
     private $ispreminum;
     
 
+
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of link
+     */ 
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set the value of link
+     *
+     * @return  self
+     */ 
+    public function setLink($link)
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+    public function getgradeCount(){
+
+        return $this->gradeCount;
+    }
+
+    public function setgradeCount($gradeCount){
+        $this->gradeCount= $gradeCount;
+        return $this;
+
+    }
+
+    /**
+     * Get the value of gradeTotalprivate
+     */ 
+    public function getGradeTotalprivate()
+    {
+        return $this->gradeTotalprivate;
+    }
+
+    /**
+     * Set the value of gradeTotalprivate
+     *
+     * @return  self
+     */ 
+    public function setGradeTotalprivate($gradeTotalprivate)
+    {
+        $this->gradeTotalprivate = $gradeTotalprivate;
+
+        return $this;
+    }
 
 }
