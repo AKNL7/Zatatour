c712de9bad36e4d94267e00d32e86f039910dfdb
octador <mutuon.j@gmail.com>
2024-02-16 14:16:32 +0100

    modif index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php 
- require_once('./utils/autoload.php');
- require_once('./utils/connexion.php');
+<?php
+require_once('./utils/autoload.php');
+require_once('./utils/connexion.php');
 
 
 
@@ -18,16 +18,15 @@ foreach ($reviews as $review) {
 $newtop = new TourOperatorManager($bdd);
 $Alloperators = $newtop->showOperator();
 
-$operators = []; 
+$operators = [];
 
 foreach ($Alloperators as $Alloperator) {
-    $operators[]= new TourOperator($Alloperator);
+    $operators[] = new TourOperator($Alloperator);
 }
 
 // var_dump($operators);
 
 foreach ($operators as $operator) {
-   
 }
 
 // Creation objets destination
@@ -49,64 +48,67 @@ var_dump($selectDestination);
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style/style.css"> 
+    <link rel="stylesheet" href="./style/style.css">
     <title>zatatour</title>
 
 </head>
+
 <body>
-   
+
     <header class="header">
-       
+
         <h1>
             Zatatour World
         </h1>
- 
+
     </header>
-   
-    
-   
+
+
+
     <h3> TOP DESTINATION</h3>
 
-        <!-- quand je clique sur envoyer en methode post le formulaire envoie id du voyage select -->
+    <!-- quand je clique sur envoyer en methode post le formulaire envoie id du voyage select -->
 
-        <form action="" method="post">
-            <label for="destination">DESTINATION : </label>
+    <form action="" method="post">
+        <label for="destination">DESTINATION : </label>
 
-            <select name="destination" id="destination">
-                <!-- <option value="">--Vers quel destination voulez-vous allez--</option> -->
-                <?php foreach ($destinations as $destination) {
-                ?> <option value="<?php echo $destination->getLocation(); ?>">
-                        <?php echo $destination->getLocation(); ?> </option>
-                <?php } ?>
-            </select>
-            <button type="submit">Envoyer</button>
-        </form>
+        <select name="destination" id="destination">
+            <!-- <option value="">--Vers quel destination voulez-vous allez--</option> -->
+            <?php foreach ($destinations as $destination) {
+            ?> <option value="<?php echo $destination->getLocation(); ?>">
+                    <?php echo $destination->getLocation(); ?> </option>
+            <?php } ?>
+        </select>
+        <button type="submit">Envoyer</button>
+    </form>
     </header>
     <section>
-    
-       
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item"><?php  ?> </li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
-        
+
+
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item"><?php  ?> </li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
