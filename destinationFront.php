dae0c744375f8c4a41af87b1221a6f56ec2f3fd0
octador <mutuon.j@gmail.com>
2024-02-21 13:21:35 +0100

    push sarah

--- destinationFront.php
+++ destinationFront.php
@@ -2,27 +2,41 @@
 
 require_once("./utils/autoload.php");
 require_once("./utils/connexion.php");
-require_once("./class/DestinationManager.php");
-require_once("./class/ReviewManager.php");
 
-$reviews = new ReviewManager($bdd); 
-$reviews->showMessage();
-// $reviews->insertReview($data);
-
-if(isset($_POST['author']) && !empty($_POST['author']) &&
-isset($_POST['message']) && !empty($_POST['message']));
-
-var_dump($_POST['message'], $_POST['author']);
-
-$createReviews = new ReviewManager($bdd); 
-$createReviews->insertReview($_POST);
+$reviews = new ReviewManager($db);
 
 
-// var_dump($reviews->showMessage());
+if (isset($_POST['destination']) && !empty(['destination'])) {
+// creation d'une instance 
+    
+     // selectioner des review avec les tour operator
+    $reviews->getReviewByOperatorId($tour_operator_id);
+}
+
+
+
+if (
+    isset($_POST['author']) && !empty($_POST['author']) &&
+    isset($_POST['message']) && !empty($_POST['message'])
+) {
+
+    echo "sa marche";
+
+    
+
+    // select toute les review
+    // $reviews->showMessage();
+    // var_dump($reviews->showMessage());
+
+    // insert des review
+    $reviews->insertReview($_POST);
+
+   
+};
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -32,37 +46,37 @@ $createReviews->insertReview($_POST);
 </head>
 
 <body class="destination">
-    <h1 class="text-center">Roma</h1>
+    <h1 class="text-center"><?php echo $_POST['location']; ?></h1>
 
     <div class="main-content">
         <div class="card_destination">
-        <div class="card-image">
-            <img src="./img/Rome.jpg">
+            <div class="card-image">
+                <img src="./img/Rome.jpg">
+            </div>
+            <div class="card-text">
+                <p class="card-meal-type"></p>
+                <h2 class="card-title">La Bella Roma</h2>
+                <p class="card-body">Eggs Benedict with hollandaise sauce, crispy bacon and an assortment of garden herbs.</p>
+            </div>
+            <div class="card-price">1650€</div>
         </div>
-        <div class="card-text">
-            <p class="card-meal-type"></p>
-            <h2 class="card-title">La Bella Roma</h2>
-            <p class="card-body">Eggs Benedict with hollandaise sauce, crispy bacon and an assortment of garden herbs.</p>
-        </div>
-        <div class="card-price">1650€</div>
+
     </div>
 
-        </div>
+    <div class="reviews">
+        <!-- <?php foreach ($reviews->showMessage() as $review) { ?>
+            <div class="comment"><?php echo $review['message'] ?></div> -->
 
-        <div class="reviews">
-        <?php foreach ($reviews->showMessage() as $review) { ?>   
-            <div class="comment"><?php echo $review['message'] ?></div>
-           
-          <?php } ?><!-- Add more comments as needed -->
-            <form class="destination_comment" action="" method="POST">
-                <label>Votre Prenom</label>
-                <input type="text" name="author" value=" <?php $createReview->insertReview() ?> ">
+        <?php } ?><!-- Add more comments as needed -->
+        <form class="destination_comment" action="" method="POST">
+            <label>Votre Prenom</label>
+            <!-- <input type="text" name="author" value=" <?php $createReview->insertReview() ?> "> -->
             <textarea class="comment" name="message">Type your comment here.</textarea>
-                <br>
-             
-                <input type="submit" name="submit" value="Send">
-            </form>
-        </div>
+            <br>
+
+            <input type="submit" name="submit" value="Send">
+        </form>
+    </div>
     </div>
 
     <!-- <div class="to_table">
@@ -80,19 +94,19 @@ $createReviews->insertReview($_POST);
             </div>
         </div> -->
 
-        <div class="pricing-box-container">
-            <div class="pricing-box text-center">
-                <h5>Fram</h5>
-                <p class="price"><sup>€</sup>1000<sub></sub></p>
-                <ul class="features-list">
-                    <li><strong>Votre Note</strong> <sub></sub></li>
-                    <li><strong>Note Total</strong></li>
-                    <li><strong>Premium Operator</strong><a href=""></a> </li>
+    <div class="pricing-box-container">
+        <div class="pricing-box text-center">
+            <h5>Fram</h5>
+            <p class="price"><sup>€</sup>1000<sub></sub></p>
+            <ul class="features-list">
+                <li><strong>Votre Note</strong> <sub></sub></li>
+                <li><strong>Note Total</strong></li>
+                <li><strong>Premium Operator</strong><a href=""></a> </li>
 
-                </ul>
-                <button class="btn-primary">Link</button>
-            </div>
+            </ul>
+            <button class="btn-primary">Link</button>
         </div>
+    </div>
     </div>
 </body>
 
--- index.php
+++ index.php
@@ -82,15 +82,10 @@ if (isset($_POST['destination']) && !empty($_POST['destination'])) {
 
 
     <section class="cards_group">
-
-
-
-
-
-
-
         <div class="main">
             <?php foreach ($newDestinations->distinctLocation() as $destination) { ?>
+
+                
                 <ul class="cards">
                     <li class="cards_item">
                         <div class="card">
@@ -102,7 +97,7 @@ if (isset($_POST['destination']) && !empty($_POST['destination'])) {
                                 <h2 class="card_title"><?php echo $destination->getLocation();  ?></h2>
                                 <div class="card_text">
                                     <p>Dig into the freshest veggies of the season! This salad-in-a-jar features a mixture of
-                                        leafy greens and seasonal vegetables, fresh from the farmer's market.
+                                        
                                     </p>
                                     <hr />
                                     <p>Served with your choice of dressing on the side: <strong>housemade ranch</strong>, <strong>cherry balsamic
@@ -115,10 +110,16 @@ if (isset($_POST['destination']) && !empty($_POST['destination'])) {
                         </div>
                     </li>
 
-                </ul>
-            <?php } ?>
+                </ul> 
+               <form action="./destinationFront.php" method="post">
+                <input type="hidden" name="location" value="<?php echo $destination->getLocation();  ?>">
+                <input type="submit" value="Submit">
+                </form>
+                
+            <?php  } ?>
         </div>
     </section>
+
 </body>
 
 </html>
